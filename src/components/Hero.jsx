a8359de33f00bdc817aacea0f3f0ed7f7f5354af
Mariana  Cruz <mda-cruz@Marianas-Air-2.Home>
2023-03-23 16:37:46 +0000

    cursed ducky

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,29 @@
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import { Canvas, useLoader } from "@react-three/fiber";
+// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
+// const duck = useLoader(GLTFLoader, "./assets/duck.glb");
+
 export default function Hero() {
+  const duck = useGLTF("./assets/duck.glb");
+  console.log(duck);
+  // const ducky = useLoader(GLTFLoader, "../assets/duck.glb");
+  // console.log(ducky);
+
   return (
     <div className="m-20">
-      <h4 className=" text-black">Hello, I'm</h4>
-      <h1 className="font-bold text-4xl">Mariana Cruz</h1>
-      <h3 className="font-bold">Creative Frontend Developer</h3>
+      <h1 className="text-black">
+        Hello, I'm <br />
+        <span className="font-bold text-4xl">Mariana Cruz</span> <br />
+        <span className="font-bold">Creative Frontend Developer</span>
+      </h1>
+      <div className="absolute h-screen w-screen">
+        <Canvas camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 0, 5] }}>
+          <ambientLight />
+          <OrbitControls />
+          <primitive object={duck.scene} scale={0.35} />
+        </Canvas>
+      </div>
     </div>
   );
 }
