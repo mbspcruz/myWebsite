9c0044dc437073dc6989d8a6e36fd5b4d9b00dc7
Mariana <mar.b.cruz@hotmail.com>
2023-03-23 15:03:34 +0000

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+  return (
+    <div className="m-20">
+      <h4 className=" text-black">Hello, I'm</h4>
+      <h1 className="font-bold text-4xl">Mariana Cruz</h1>
+      <h3 className="font-bold">Creative Frontend Developer</h3>
+    </div>
+  );
+}
